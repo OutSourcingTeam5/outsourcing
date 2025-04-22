7f42451d3ff2664bc53d9fd2c9fdef2666007807
HeeMang-Lee <133612543+HeeMang-Lee@users.noreply.github.com>
2025-04-22 20:58:51 +0900

    Domain2 (#2)
    * domian init
    
    * domain init 2

---
+++ src/main/java/com/example/outsourcing/domain/Auth/dto/AuthRequestDto.java
@@ -0,0 +1,4 @@
+package com.example.outsourcing.domain.Auth.dto;
+
+public class AuthRequestDto {
+}
---
+++ src/main/java/com/example/outsourcing/domain/User/dto/UserRequestDto.java
@@ -0,0 +1,4 @@
+package com.example.outsourcing.domain.User.dto;
+
+public class UserRequestDto {
+}
