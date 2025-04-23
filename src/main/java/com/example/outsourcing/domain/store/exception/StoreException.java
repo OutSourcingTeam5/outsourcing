b4441d75d9e63759fef95a83e3ca1fc333518d6a
AhyunCho <jah112482@gmail.com>
2025-04-23 20:53:51 +0900

    fix : requestDto 타입 변환

---
+++ src/main/java/com/example/outsourcing/domain/store/exception/StoreException.java
@@ -0,0 +1,14 @@
+package com.example.outsourcing.domain.store.exception;
+
+import com.example.outsourcing.common.exception.BaseCode;
+import com.example.outsourcing.common.exception.CustomException;
+
+public class StoreException extends CustomException {
+    public StoreException(BaseCode baseCode) {
+        super(baseCode);
+    }
+
+    public StoreException(BaseCode baseCode, String message) {
+        super(baseCode, message);
+    }
+}
