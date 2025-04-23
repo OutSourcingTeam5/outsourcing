ae68cc9205c44945570a470b7cf6a5acb68b8cd0
HyunJG <ksw9250@naver.com>
2025-04-23 20:50:14 +0900

    [Feat] Order Entity 구현 , SuccessCode Code 복구 (#11)
    * [FEAT] Order Entity 추가
    
    * [fix] SuccessCode 복구

--- src/main/java/com/example/outsourcing/common/exception/SuccessCode.java
+++ src/main/java/com/example/outsourcing/common/exception/SuccessCode.java
@@ -1,17 +1,17 @@
 package com.example.outsourcing.common.exception;
 
+import org.springframework.http.HttpStatus;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import org.springframework.http.HttpStatus;
 
 @Getter
 @AllArgsConstructor
 public enum SuccessCode implements BaseCode {
+	OK(HttpStatus.OK, "S001", "요청을 성공했습니다."),
+	CREATED(HttpStatus.CREATED, "S002", "요청이 생성되었습니다.");
 
-    OK(HttpStatus.OK, "S001", "요청을 성공했습니다."),
-    CREATED(HttpStatus.CREATED, "S002", "요청이 생성되었습니다.");
-
-    private final HttpStatus httpStatus;
-    private final String code;
-    private final String message;
+	private final HttpStatus httpStatus;
+	private final String code;
+	private final String message;
 }
